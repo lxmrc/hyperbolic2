a04896476b9a4843e9dd6536d8a5a7d6597d5aa3
Alex Mercea <alex@lxmrc.com>
2023-06-24 11:32:16 +1000

    Remove first name and last name from users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_23_050019) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_24_013124) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -117,8 +117,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_23_050019) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "first_name"
-    t.string "last_name"
     t.datetime "announcements_last_read_at"
     t.boolean "admin", default: false
     t.datetime "created_at", null: false
