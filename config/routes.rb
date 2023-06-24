cffdf76668d9a10dbbdcd627014f962345ccca02
Alex Mercea <alex@lxmrc.com>
2023-06-24 11:58:29 +1000

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,22 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  unauthenticated do
+    root to: 'home#index'
+  end
+
+  authenticate :user do
+    root to: 'exercises#index', as: :authenticated_root
+  end
+
+  resources :exercises do
+    resources :iterations do
+      collection do
+        post :run
+      end
+    end
+  end
+
   resources :containers do
     collection do
       post :destroy_all
@@ -12,41 +28,19 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :exercises do
-    resources :iterations do
-      collection do
-        post :run
-      end
-    end
-  end
+  # Things from the Jumpstart Rails template which I don't need right now
+  # authenticate :user, lambda { |u| u.admin? } do
+  #   mount Sidekiq::Web => '/sidekiq'
 
-  get '/privacy', to: 'home#privacy'
-  get '/terms', to: 'home#terms'
+  #   namespace :madmin do
+  #     resources :impersonates do
+  #       post :impersonate, on: :member
+  #       post :stop_impersonating, on: :collection
+  #     end
+  #   end
+  # end
 
-  unauthenticated do
-    root to: 'home#index'
-  end
-
-  authenticate :user do
-    root to: 'exercises#index', as: :authenticated_root
-  end
-
-  authenticate :user, lambda { |u| u.admin? } do
-    mount Sidekiq::Web => '/sidekiq'
-
-    namespace :madmin do
-      resources :impersonates do
-        post :impersonate, on: :member
-        post :stop_impersonating, on: :collection
-      end
-    end
-  end
-
-  resources :notifications, only: [:index]
-  resources :announcements, only: [:index]
+  # resources :notifications, only: [:index]
+  # resources :announcements, only: [:index]
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
