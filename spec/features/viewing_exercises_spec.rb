4e442cb4fe1f878b81af413059868a39223d4eed
Alex Mercea <alex@lxmrc.com>
2023-06-24 12:21:39 +1000

    Update viewing exercises spec

--- spec/features/viewing_exercises_spec.rb
+++ spec/features/viewing_exercises_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 
 RSpec.feature 'Users can view exercises', type: :feature do
   let!(:exercise1) { FactoryBot.create(:exercise) }
+  let(:user) { FactoryBot.create(:user) }
+  let(:admin) { FactoryBot.create(:user, admin: true) }
 
-  scenario 'when not authenticated' do
+  scenario 'when not logged in' do
     visit '/'
 
     expect(page).to have_content exercise1.name
@@ -18,5 +20,43 @@ RSpec.feature 'Users can view exercises', type: :feature do
 
     expect(page).to_not have_link 'Attempt'
     expect(page).to_not have_link 'Edit'
+  end
+
+  scenario 'when logged in as a regular user' do
+    login_as(user)
+
+    visit '/'
+
+    expect(page).to have_content exercise1.name
+    expect(page).to have_content exercise1.description.truncate_words(30, omission: '...')
+    expect(page).to_not have_link 'Create Exercise'
+
+    click_link exercise1.name
+
+    expect(page).to have_content exercise1.name
+    expect(page).to have_content exercise1.description
+    expect(page).to have_content exercise1.tests
+
+    expect(page).to have_link 'Attempt'
+    expect(page).to have_link 'Edit'
+  end
+
+  scenario 'when logged in as an admin ' do
+    login_as(admin)
+
+    visit '/'
+
+    expect(page).to have_content exercise1.name
+    expect(page).to have_content exercise1.description.truncate_words(30, omission: '...')
+    expect(page).to have_link 'Create Exercise'
+
+    click_link exercise1.name
+
+    expect(page).to have_content exercise1.name
+    expect(page).to have_content exercise1.description
+    expect(page).to have_content exercise1.tests
+
+    expect(page).to have_link 'Attempt'
+    expect(page).to have_link 'Edit'
   end
 end
