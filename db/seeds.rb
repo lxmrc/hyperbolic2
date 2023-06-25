08b30258bab7af640e64e735730c8824f1d8f92c
Alex Mercea <alex@lxmrc.com>
2023-06-25 11:04:05 +1000

    Add seed exercise

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,83 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+greed_description = <<~TEXT
+  Greed is a dice game where you roll up to five dice to accumulate
+  points.  The following "score" function will be used to calculate the
+  score of a single roll of the dice.
+
+  A greed roll is scored as follows:
+
+  * A set of three ones is 1000 points
+
+  * A set of three numbers (other than ones) is worth 100 times the
+    number. (e.g. three fives is 500 points).
+
+  * A one (that is not part of a set of three) is worth 100 points.
+
+  * A five (that is not part of a set of three) is worth 50 points.
+
+  * Everything else is worth 0 points.
+
+
+  Examples:
+
+  score([1,1,1,5,1]) => 1150 points
+  score([2,3,4,6,2]) => 0 points
+  score([3,4,5,3,3]) => 350 points
+  score([1,5,1,2,4]) => 250 points
+
+  More scoring examples are given in the tests below:
+
+  Your goal is to write the score method.
+TEXT
+
+greed_tests = <<~RUBY
+  require 'minitest/autorun'
+  require 'minitest/reporters'
+  require 'minitest/reporters/json_reporter'
+  Minitest::Reporters.use! Minitest::Reporters::JsonReporter.new
+
+  require_relative "exercise"
+
+  class GreedTest < Minitest::Test
+    def test_score_of_an_empty_list_is_zero
+      assert_equal 0, Greed.score([])
+    end
+
+    def test_score_of_a_single_roll_of_5_is_50
+      assert_equal 50, Greed.score([5])
+    end
+
+    def test_score_of_a_single_roll_of_1_is_100
+      assert_equal 100, Greed.score([1])
+    end
+
+    def test_score_of_multiple_1s_and_5s_is_the_sum_of_individual_scores
+      assert_equal 300, Greed.score([1,5,5,1])
+    end
+
+    def test_score_of_single_2s_3s_4s_and_6s_are_zero
+      assert_equal 0, Greed.score([2,3,4,6])
+    end
+
+    def test_score_of_a_triple_1_is_1000
+      assert_equal 1000, Greed.score([1,1,1])
+    end
+
+    def test_score_of_other_triples_is_100x
+      assert_equal 200, Greed.score([2,2,2])
+      assert_equal 300, Greed.score([3,3,3])
+      assert_equal 400, Greed.score([4,4,4])
+      assert_equal 500, Greed.score([5,5,5])
+      assert_equal 600, Greed.score([6,6,6])
+    end
+
+    def test_score_of_mixed_is_sum
+      assert_equal 250,  Greed.score([2,5,2,2,3])
+      assert_equal 550,  Greed.score([5,5,5,5])
+      assert_equal 1100, Greed.score([1,1,1,1])
+      assert_equal 1200, Greed.score([1,1,1,1,1])
+      assert_equal 1150, Greed.score([1,1,1,5,1])
+    end
+  end
+RUBY
+
+greed = Exercise.create(name: "Greed", description: greed_description, tests: greed_tests)
